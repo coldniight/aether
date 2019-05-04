2b98c194b3f1e70de012cc6f8e65255939dbc18a
coldniight <49598352+coldniight@users.noreply.github.com>
2019-05-04 13:47:35 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
     let botembed = new Discord.RichEmbed()
     .setTitle("Aether Bot")
     .setDescription(`Pong! \`${Math.round(bot.ping)}\` ms.`)
-    .setColor(0x00fffa)
+    .setColor(0xff2b2b)
     .setFooter(message.id)
     .setTimestamp();
     message.channel.send(botembed);
