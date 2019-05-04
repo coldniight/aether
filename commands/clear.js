a1300aa7d0785912cab5a26542deae986fcffd23
coldniight <49598352+coldniight@users.noreply.github.com>
2019-05-04 13:45:49 -0500

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -5,14 +5,14 @@ module.exports.run = async (bot, message, args) => {
     let noperm = new Discord.RichEmbed()
     .setTitle("Aether Bot")
     .setDescription(`You don't have enough permissions to see that command, ${message.author}.`)
-    .setColor(0x00fffa)
+    .setColor(0xff2b2b)
     .setFooter(message.id)
     .setTimestamp();
 
     let helpembed = new Discord.RichEmbed()
     .setTitle("Aether Bot")
     .setDescription(`Usage of command **clear**\n\n**Usage:** $clear <amount>\n**Description:** Deletes an amount of messages.`)
-    .setColor(0x00fffa)
+    .setColor(0xff2b2b)
     .setFooter(message.id)
     .setTimestamp();
 
@@ -25,7 +25,7 @@ module.exports.run = async (bot, message, args) => {
     let messageembed = new Discord.RichEmbed()
     .setTitle("Aether Bot")
     .setDescription(`Please supply a number of messages to delete, ${message.author}.`)
-    .setColor(0x00fffa)
+    .setColor(0xff2b2b)
     .setFooter(message.id)
     .setTimestamp();
 
